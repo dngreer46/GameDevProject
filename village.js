4441727f365e2f8f8496334089d91b777024dde7
Cathy Li <cathysli@outlook.com>
2018-10-05 15:48:54 -0500

    add village.js
    implemented into index.html and main.js

---
+++ village.js
@@ -0,0 +1,23 @@
+demo.village = function(){};
+
+demo.village.prototype = {
+    
+    preload: function(){
+        game.stage.backgroundColor = "#000"
+        
+        // Load tileset images
+        game.load.image('natureSet', 'assets/maps/natureSet.png');
+        game.load.image('houseSet', 'assets/maps/house2.png');
+        game.load.image('castleSet1', 'assets/maps/castleSet1.png');
+    },
+    
+    create: function(){
+        
+    },
+    
+    update: function(){
+        
+    }
+    
+    // Functions
+};
