9e6772a33d663c732459b3860711c1c596546c07
Cathy Li <cathysli@outlook.com>
2018-10-05 16:22:59 -0500

    Add tilemap to game

--- village.js
+++ village.js
@@ -1,17 +1,34 @@
+var map, groundLayer, platformsLayer, houseLayer, plantsSignsLayer, ladderLayer, chestLayer
+
+var demo = {};
 demo.village = function(){};
 
 demo.village.prototype = {
     
     preload: function(){
-        game.stage.backgroundColor = "#000"
         
-        // Load tileset images
+        // Preload tileset images
         game.load.image('natureSet', 'assets/maps/natureSet.png');
         game.load.image('houseSet', 'assets/maps/house2.png');
-        game.load.image('castleSet1', 'assets/maps/castleSet1.png');
+        
+        // Preload tilemap
+        game.load.tilemap('villageMap', 'assets/maps/villageMap.json', null, Phaser.Tilemap.TILED_JSON);
+        
     },
     
     create: function(){
+        
+        // Load map
+        map = game.add.tilemap('villageMap');
+        map.addTilesetImage('natureSet');
+        map.addTilesetImage('houseSet');
+        
+        groundLayer = map.createLayer('Ground');
+        platformsLayer = map.createLayer('Platforms');
+        houseLayer = map.createLayer('Houses');
+        plantsSignsLayer = map.createLayer('PlantsSigns');
+        ladderLayer = map.createLayer('Ladders');
+        chestLayer = map.createLayer('Chests');
         
     },
     
