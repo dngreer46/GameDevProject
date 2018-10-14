9eb9fd5641b24f72799a65048a33d1a566c24d99
Cathy Li <cathysli@outlook.com>
2018-10-14 11:23:31 -0500

    Forest Map
    - Added forest map + basic physics
    - Moved platform physic function to main.js

---
+++ Assets/Maps/natureSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="natureSet" tilewidth="32" tileheight="32" tilecount="60" columns="10">
+ <image source="../../../../../Dropbox/School/CS 329E Game Dev/Maps/natureSet.png" width="320" height="192"/>
+</tileset>
--- main.js
+++ main.js
@@ -2,4 +2,51 @@ var game = new Phaser.Game(800, 500, Phaser.AUTO);
 game.state.add('bossState', demo.bossState);
 game.state.add('village', demo.village);
 game.state.add('youDied', demo.youDied);
-game.state.start('village');
+game.state.add('forest', demo.forest);
+game.state.start('forest');
+
+// Allows collision for select tile faces
+// Found here: https://thoughts.amphibian.com/2015/11/single-direction-collision-for-your.html
+function setTileCollision(mapLayer, idxOrArray, dirs) {
+
+    var mFunc; // tile index matching function
+    if (idxOrArray.length) {
+        // if idxOrArray is an array, use a function with a loop
+        mFunc = function(inp) {
+            for (var i = 0; i < idxOrArray.length; i++) {
+                if (idxOrArray[i] === inp) {
+                    return true;
+                }
+            }
+            return false;
+        };
+    } else {
+        // if idxOrArray is a single number, use a simple function
+        mFunc = function(inp) {
+            return inp === idxOrArray;
+        };
+    }
+
+    // get the 2-dimensional tiles array for this layer
+    var d = mapLayer.map.layers[mapLayer.index].data;
+
+    for (var i = 0; i < d.length; i++) {
+        for (var j = 0; j < d[i].length; j++) {
+            var t = d[i][j];
+            if (mFunc(t.index)) {
+
+                t.collideUp = dirs.top;
+                t.collideDown = dirs.bottom;
+                t.collideLeft = dirs.left;
+                t.collideRight = dirs.right;
+
+                t.faceTop = dirs.top;
+                t.faceBottom = dirs.bottom;
+                t.faceLeft = dirs.left;
+                t.faceRight = dirs.right;
+
+            }
+        }
+    }
+
+}
