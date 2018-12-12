abe4d60888409364fd3a8a7f909b8d8dfefa9bda
Samuel Bernasconi <bernasconisamuel@gmail.com>
2018-12-12 12:03:58 -0600

    ending with credits

--- gameEnd.js
+++ gameEnd.js
@@ -6,13 +6,23 @@ demo.gameEnd.prototype = {
     create: function(){
         game.stage.backgroundColor = "#000"
             
-        var text = game.add.text(400, 250, 'You defeated the lab monster\n     and saved your sister!', {fontSize: '36px', fill: '#fff'});
+        var text = game.add.text(400, 100, 'You defeated the lab monster\n     and saved your sister!', {fontSize: '36px', fill: '#fff'});
         text.anchor.x = 0.5;
         text.anchor.y = 0.5;
         
-        text = game.add.text(400, 400, 'THE END', {fontSize: '18px', fill: '#fff'});
+        text = game.add.text(400, 200, 'THE END', {fontSize: '18px', fill: '#fff'});
         text.anchor.x = 0.5;
         text.anchor.y = 0.5;
+        
+        text = game.add.text(400, 250, 'Producer: Paul Toprac', {fontSize: '18px', fill: '#fff'});
+        text.anchor.x = 0.5;
+        text.anchor.y = 0.5;
+        
+        
+        text = game.add.text(400, 300, 'Developers: Danny Greer, Cathy Li, Sam Bernasconi', {fontSize: '18px', fill: '#fff'});
+        text.anchor.x = 0.5;
+        text.anchor.y = 0.5;
+        
     },
     update: function(){}
 };
